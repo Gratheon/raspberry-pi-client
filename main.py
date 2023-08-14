bc22aa7a1e2d675f4c0ab2a7a6e4fdbbde61845b
Artjom Kurapov <artkurapov@gmail.com>
2023-08-14 21:54:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
+import os
 import time
-import picamera
 import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import picamera
 
 # Configure the camera
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)  # Adjust resolution as needed
 
+# Start the camera preview
+camera.start_preview()
+
 try:
     while True:
         # Record video
-        output_file = f'video_{time.time()}.h264'
+        output_file = f'video_{time.time()}.h264'  # Use .h264 extension
         camera.start_recording(output_file)
         camera.wait_recording(5)  # Record for 5 seconds
         camera.stop_recording()
 
-        # Upload the recorded video
+        # Prepare multipart/form-data request
         url = 'https://video.gratheon.com/graphql'
-
         headers = {
-            'Content-Type': 'application/json',
             'Authorization': 'Bearer YOUR_ACCESS_TOKEN'
         }
-        query = '''
-        mutation UploadVideo($file: Upload!, $boxId: ID!) {
-            uploadGateVideo(file: $file, boxId: $boxId)
+        payload = {
+            'query': (
+                'mutation UploadVideo($file: Upload!, $boxId: ID!) {'
+                '  uploadGateVideo(file: $file, boxId: $boxId)'
+                '}'
+            ),
+            'variables': {
+                'boxId': '123'  # Replace with the actual box ID
+            }
         }
-        '''
-        variables = {
-            'file': (output_file, open(output_file, 'rb')),
-            'boxId': '123'  # Replace with the actual box ID
+        files = {
+            'file': (output_file, open(output_file, 'rb'), 'video/mp4')  # Adjust content type if needed
         }
-        data = {
-            'query': query,
-            'variables': variables
-        }
+        multipart_data = MultipartEncoder(fields=files, boundary='----MyBoundary')
 
-        # Make GraphQL request
-        response = requests.post(url, headers=headers, json=data)
+        # Make multipart/form-data request
+        response = requests.post(url, headers=headers, data=multipart_data,
+                                 params=payload, timeout=30, allow_redirects=True)
 
         if response.status_code == 200:
             print("Video uploaded successfully")
-            os.remove(output_file) 
+            os.remove(output_file)
         else:
             print("Error uploading video:", response.status_code)
 
@@ -48,4 +53,5 @@ except KeyboardInterrupt:
     print("Recording and uploading stopped by user")
 
 finally:
+    camera.stop_preview()  # Stop the camera preview
     camera.close()
